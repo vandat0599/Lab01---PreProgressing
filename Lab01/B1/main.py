e3ca0e577a87594c89794829b7ffd2c7fb1ae540
Đạt <vovandat28051999@gmail.com>
2019-09-30 01:11:45 +0700

    done 2

--- Lab01/B1/main.py
+++ Lab01/B1/main.py
@@ -1,36 +1,54 @@
+import csv
+import sys
 from CountryOb import *
 
-x = open("countries.txt")
-countries = []
-data = list(x)
-it = iter(data)
-line = next(it)
-try:
-    while True:
-        countryOb = CountryOb()
-        attr = line[0:line.index("=")]
-
-        countryOb.attr = line[line.index("=") + 1: len(line)]
-        # print(countryOb.attr)
-        line = next(it)
-        while "country" not in line:
-            attr = line[0:line.index("=")]
-            countryOb.attr = line[line.index("=") + 1: len(line)]
+# valid command: preprocess --input {inputFilePath} --output {outputFileCSVPath}
+argvList = sys.argv
+if "--input" in argvList and "--output" in argvList and "preprocess" in argvList and ".csv" in argvList[len(argvList)-1]:
+    x = open(argvList[3], encoding='utf-8-sig')
+    countries = []
+    data = list(x)
+    it = iter(data)
+    line = next(it)
+    try:
+        while True:
+            country = {}
+            lineSplit = line.rstrip('\n').split("=")
+            country.update({lineSplit[0]: lineSplit[1]})
             line = next(it)
-        countries.append(countryOb)
-except:
-    print("Out it")
+            while "country" not in line:
+                lineSplit = line.rstrip('\n').split("=")
+                country.update({lineSplit[0]: lineSplit[1]})
+                line = next(it)
+            # remove empty data "country,name,longName,foundingDate,population,capital,largestCity,area"
+            if country.get("name") is not None and country.get("longName") is not None and country.get("foundingDate") is not None and country.get("population") is not None and country.get("capital") is not None and country.get("largestCity") is not None and country.get("area") is not None:
+                # change mile to km
+                area = country["area"]
+                if area is not None and "mi" in area and country["country"] != "country":
+                    country["area"] = "{}km".format(
+                        float(area[0:len(area)-2])*2.59)
+                # remove duplicate data
+                isDuplicate = False
+                for c in countries:
+                    # cCopy["country"] = country["country"]
+                    if c["name"] == country["name"] and c["longName"] == country["longName"] and c["foundingDate"] == country["foundingDate"] and c["population"] == country["population"] and c["capital"] == country["capital"] and c["largestCity"] == country["largestCity"] and c["area"] == country["area"]:
+                        isDuplicate = True
+                        break
+                if not(isDuplicate):
+                    countries.append(country)
 
-print(countries[0].attr)
-# it = iter(x)
-# while():
-#     print(next(it))
-# for i in x:
-#     countryOb = CountryOb()
-#     attr = i[0:i.index("=")]
-#     countryOb.attr = i[i.index("=") + 1: len(i)]
-#     if "country" in i:
+    except:
+        print("Read data done!!!")
 
-# x = CountryOb()
-# x.a = 10
-# print(hasattr(x, "a"))
+    try:
+        with open(argvList[5], 'w') as csvfile:
+            writer = csv.DictWriter(
+                csvfile, fieldnames=list(countries[0].keys()))
+            writer.writeheader()
+            for i in range(len(countries)-1):
+                writer.writerow(countries[i+1])
+    except IOError:
+        print("I/O error")
+else:
+    print(
+        "---Command not found\n---Valid command: preprocess --input {Input} --output {Output}\n-----Input: data file (.txt,...)\n-----Ouput: *.csv")
